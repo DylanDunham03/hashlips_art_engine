3c4bf410322d1332f0726b060abada31f6bacd22
DylanDunham03 <dylanad2@illinois.edu>
2021-12-19 22:21:24 -0500

    NFT setup

--- src/config.js
+++ src/config.js
@@ -24,15 +24,14 @@ const solanaMetadata = {
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 50,
     layersOrder: [
       { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Base" },
+      { name: "Texture" },
+      { name: "Eyes" },
+      { name: "Mouth" },
+      { name: "Hats" },
     ],
   },
 ];
@@ -42,8 +41,8 @@ const shuffleLayerConfigurations = false;
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 2100,
+  height: 2100,
   smoothing: false,
 };
 
